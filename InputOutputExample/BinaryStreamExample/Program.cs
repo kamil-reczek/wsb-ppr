34a06c2c16229feb4fd6612b038cfe61e4833ca0
kamil <48632268+kamil-reczek@users.noreply.github.com>
2019-11-24 08:44:17 +0100

    Adding text and binary stream

---
+++ InputOutputExample/BinaryStreamExample/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace BinaryStreamExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FileInfo outputFile = new FileInfo(@"C:\testDir\textFile.dat");
+
+
+            // Write a binary file
+            string hello = "Hello World";
+            char[] chars = hello.ToCharArray();
+            try 
+            {
+                using (BinaryWriter bw = new BinaryWriter(outputFile.Open(FileMode.OpenOrCreate, FileAccess.Write), Encoding.UTF8))
+                {
+                    foreach(char ch in chars)
+                    {
+                        bw.Write(ch);
+                    }
+                    
+                }
+             } 
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+
+            // Read binary file and display the content in console
+            byte temp;
+            try
+            {
+                using (BinaryReader br = new BinaryReader(outputFile.Open(FileMode.Open, FileAccess.Read)))
+                {
+                    while ((temp = br.ReadByte()) != null)
+                    {
+                        Console.WriteLine(temp);
+                    }
+                }
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
---
+++ InputOutputExample/TextStreamExample/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Threading;
+
+namespace TextStreamExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Get text data from file and display it on the console
+            FileInfo inputFile = new FileInfo(@"C:\TestDir\textFile.txt");
+            string temp;
+            try
+            {
+                using (TextReader tr = inputFile.OpenText())
+                {
+                    while ((temp = tr.ReadLine()) != null)
+                    {
+                        Console.WriteLine(temp);
+                    }
+                }
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            // Create new file and append data (current time in unit timestamp)
+            FileInfo outputFile = new FileInfo(@"C:\TestDir\output.txt");
+            try
+            {
+                using (TextWriter tw = outputFile.CreateText())
+                {
+                    for (int i = 0; i < 10; i++)
+                    {
+                        tw.WriteLine(DateTimeOffset.Now.ToUnixTimeMilliseconds());
+                        Thread.Sleep(500);
+                    }
+                }
+            } catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            // Append new data to existing file
+            try
+            {
+                using(TextWriter tw = outputFile.AppendText())
+                {
+                    tw.WriteLine();
+                    tw.WriteLine("Author: Krzysztof Jarzyna ze Szczecina");
+                }
+            } catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+                
+        }
+    }
+}
