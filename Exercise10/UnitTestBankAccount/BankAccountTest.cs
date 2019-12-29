098b59e22774213559be25b86bb2dd2590b51193
kamil <48632268+kamil-reczek@users.noreply.github.com>
2019-12-29 17:47:25 +0100

    Adding exercise 10 - Unit testing banking app

---
+++ Exercise10/UnitTestBankAccount/BankAccountTest.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestBankAccount
+{
+    [TestClass]
+    public class BankAccountTest
+    {
+        [TestInitialize]
+        public void SetUp()
+        {
+            BankExample.BankAccount.Deposit(100);
+        }
+
+        [TestMethod]
+        public void TestWitdrawal()
+        {
+            // Make possible withdrawal
+            BankExample.BankAccount.Withdraw(40);
+            Assert.AreEqual(60, BankExample.BankAccount.GetBalance());
+
+            // Use negative amount
+            try
+            {
+                BankExample.BankAccount.Withdraw(-1);
+                Assert.Fail("No exception thrown");  // if the above method won't throw exception it means the test failed.
+            } catch(Exception e)
+            {
+                Assert.IsTrue(e is ArgumentOutOfRangeException);
+                string msg = e.Message;
+                Assert.IsTrue(msg.Contains("Amount for withdrawal is less than 0"));
+                Assert.AreEqual(60, BankExample.BankAccount.GetBalance());
+            }
+
+            // Withdraw more than the balance
+            try
+            {
+                BankExample.BankAccount.Withdraw(100);
+                Assert.Fail("No exception thrown");
+            } catch(Exception e)
+            {
+                Assert.IsTrue(e is ArgumentOutOfRangeException);
+                string msg = e.Message;
+                Assert.IsTrue(msg.Contains("Insufficient balance!"));
+                Assert.AreEqual(60, BankExample.BankAccount.GetBalance());
+            }
+        }
+
+        [TestMethod]
+        public void TestDeposit()
+        {
+            // Make possible Deposit
+            BankExample.BankAccount.Deposit(60);
+            Assert.AreEqual(160, BankExample.BankAccount.GetBalance());
+
+            // Use negative amount
+            try
+            {
+                BankExample.BankAccount.Deposit(-1);
+                Assert.Fail("No exception thrown");  // if the above method won't throw exception it means the test failed.
+            }
+            catch (Exception e)
+            {
+                Assert.IsTrue(e is ArgumentOutOfRangeException);
+                string msg = e.Message;
+                Assert.IsTrue(msg.Contains("Amount for deposit is less than 0"));
+                Assert.AreEqual(160, BankExample.BankAccount.GetBalance());
+            }
+        }
+
+        [TestCleanup]
+        public void OnTearDown()
+        {
+            BankExample.BankAccount.Withdraw(BankExample.BankAccount.GetBalance());
+        }
+    }
+}
---
+++ Exercise10/UnitTestBankAccount/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestBankAccount")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestBankAccount")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("4855f426-2bf1-401e-95a2-b62778bc8163")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
